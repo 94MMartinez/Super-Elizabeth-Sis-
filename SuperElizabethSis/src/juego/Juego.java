a5a3e5d661e78142ab1b993d0a91530eaf9db925
Martinez Maximiliano <mm.martinezmaximiliano@gmail.com>
2020-06-25 00:46:38 -0300

    puntaje
    aumenta el puntaje al disparar al soldado

--- SuperElizabethSis/src/juego/Juego.java
+++ SuperElizabethSis/src/juego/Juego.java
@@ -19,6 +19,7 @@ public class Juego extends InterfaceJuego
 	private int puntos;
     private boolean juegoFin = false;
 	private LinkedList <Boladefuego> listaBol;
+	
 
 	
 	
@@ -34,13 +35,12 @@ public class Juego extends InterfaceJuego
 	this.piso=new Piso(400,579,100,800);
 	}
 	Juego()
-	{	vidas = 3;
-	   puntos= 0;
+	{	
+		
+		vidas = 3;
+	    puntos= 0;
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Super Elizabeth Sis - Grupo 13", 800, 600);
-
-
-
 		this.prin = new Princesa(150,500,60,30);
 		this.obs= new Obstaculos(825,500,20,30);
 		this.sold= new Soldados(800,500,53,20);
@@ -103,6 +103,7 @@ public class Juego extends InterfaceJuego
 		for(Boladefuego bola:listaBol) {
 			if (sold.getX()-sold.getAnchosoldado()/2 < bola.getX()+bola.getAncho()/2 && sold.getX()-sold.getAnchosoldado()/2 > bola.getX()-bola.getAncho()/2) {
 				listaBol.remove(bola);
+				puntos = puntos+1;
 				
 			}
 	    }
@@ -117,7 +118,8 @@ public class Juego extends InterfaceJuego
 				vidas = vidas -1;
 				System.out.println(vidas);
 				if(vidas==0) {
-				 entorno.escribirTexto("F", 325, 200);
+					this.entorno.iniciar();
+					entorno.escribirTexto("F", 325, 200);
 				}
 	      }			 		   
 
